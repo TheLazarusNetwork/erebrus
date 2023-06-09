84df02fc524c3efc0ba12b4d1192c0d75e1d0a58
inciner8r <punarvpawade17@gmail.com>
2023-06-09 15:27:45 +0530

    send hello to given multiaddr

--- util/pkg/node/node.go
+++ util/pkg/node/node.go
@@ -1,17 +1,31 @@
 package node
 
 import (
+	"bufio"
+	"context"
 	"fmt"
+	"io"
+	"log"
 
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/peerstore"
+	ma "github.com/multiformats/go-multiaddr"
 )
 
 func Init() {
-	node, err := libp2p.New()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	node, err := libp2p.New(
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/2001"),
+	)
 	if err != nil {
 		panic(err)
 	}
-
+	runSender(ctx, node, "/ip4/127.0.0.1/tcp/2000/p2p/12D3KooWM628mb9VkZAPDAWgZgPtYx7YfKhrZZUawee9cEQFc8Q5")
 	// print the node's listening addresses
 	fmt.Println("Listen addresses:", node.Addrs())
 
@@ -19,4 +33,75 @@ func Init() {
 	if err := node.Close(); err != nil {
 		panic(err)
 	}
+
+}
+
+func runSender(ctx context.Context, ha host.Host, targetPeer string) {
+	// Set a stream handler on host A. /echo/1.0.0 is
+	// a user-defined protocol name.
+	ha.SetStreamHandler("/echo/1.0.0", func(s network.Stream) {
+		log.Println("sender received new stream")
+		if err := doEcho(s); err != nil {
+			log.Println(err)
+			s.Reset()
+		} else {
+			s.Close()
+		}
+	})
+
+	// Turn the targetPeer into a multiaddr.
+	maddr, err := ma.NewMultiaddr(targetPeer)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Extract the peer ID from the multiaddr.
+	info, err := peer.AddrInfoFromP2pAddr(maddr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// We have a peer ID and a targetAddr so we add it to the peerstore
+	// so LibP2P knows how to contact it
+	ha.Peerstore().AddAddrs(info.ID, info.Addrs, peerstore.PermanentAddrTTL)
+
+	log.Println("sender opening stream")
+	// make a new stream from host B to host A
+	// it should be handled on host A by the handler we set above because
+	// we use the same /echo/1.0.0 protocol
+	s, err := ha.NewStream(context.Background(), info.ID, "/echo/1.0.0")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("sender saying hello")
+	_, err = s.Write([]byte("Hello, world!\n"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	out, err := io.ReadAll(s)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Printf("read reply: %q\n", out)
+}
+
+// doEcho reads a line of data a stream and writes it back
+func doEcho(s network.Stream) error {
+	buf := bufio.NewReader(s)
+	str, err := buf.ReadString('\n')
+	if err != nil {
+		return err
+	}
+
+	log.Printf("read: %s", str)
+	_, err = s.Write([]byte(str))
+	return err
 }
